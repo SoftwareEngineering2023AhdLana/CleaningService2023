526904aaa927a1c00e5c797d0fc2a8fc4cf5c698
Ahd Ghazal <ahdgh02@gmail.com>
2023-05-12 14:51:12 +0300

    test

--- cleaningService/src/test/java/cleaningService/acceptanceTest.java
+++ cleaningService/src/test/java/cleaningService/acceptanceTest.java
@@ -1,15 +1,16 @@
 package cleaningService;
 
 
+import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
-import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "cases",monochrome= true, 
 snippets = SnippetType.CAMELCASE, 
-glue = {"cleaningService2023"},	
+glue = {"cleaningService"},	
 plugin = {"html:target/cucumber/wikipedia.html"})
 
 public class acceptanceTest {
