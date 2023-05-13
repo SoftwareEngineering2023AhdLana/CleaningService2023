157ca836e36ec1d7d5051c182a2a9623dbdfefc8
Lana Hasan <124264830+LanaHasan@users.noreply.github.com>
2023-05-14 00:54:01 +0300

    Update WorkerFunction.java

--- cleaningService/src/main/java/com/cleaning_service/WorkerFunction.java
+++ cleaningService/src/main/java/com/cleaning_service/WorkerFunction.java
@@ -10,7 +10,31 @@ public class WorkerFunction {
 	}
 	protected static List<Worker> workers=new ArrayList<Worker>();
 	protected static List<Product> products=new ArrayList<Product>();
+	public   void findAvilableWorker() {
+		String n="no available worker... ";
+     	boolean b = false;
+		LocalTime currentTime = LocalTime.now();
+        System.out.println("Current time is: " + currentTime);
+        LocalTime start,end;
+		for(int j = 0; j < 6 ;j++) {
+		
+		      System.out.println(workers.get(j).getTimeStart());
+		      System.out.println(workers.get(j).getTimeEnd());
 
+			   start = LocalTime.of( Integer.parseInt(workers.get(j).getTimeStart().split(":")[0]) ,Integer.parseInt(workers.get(j).getTimeStart().split(":")[1])); 
+		        System.out.println("start time is: " + start);
+			   end = LocalTime.of(Integer.parseInt(workers.get(j).getTimeEnd().split(":")[0]), Integer.parseInt(workers.get(j).getTimeEnd().split(":")[1])); 
+		        System.out.println("end time is: " + end);
+
+		        if (currentTime.isAfter(start) &&   (currentTime.isBefore(end)) ){
+		        	System.out.println(workers.get(j).getName());
+                 b= true;
+               		break;
+		        }
+		      
+		}
+		if(!b)System.out.println(n);
+	}
 	//search worker By Name 
 	public static int search(String w) {
 		for(int i=0; i < workers.size() ; i++) {
