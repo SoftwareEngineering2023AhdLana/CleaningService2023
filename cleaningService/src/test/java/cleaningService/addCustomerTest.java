526f9c0071290bfde45a04272cc57f08cfa0d122
Lana Hasan <124264830+LanaHasan@users.noreply.github.com>
2023-05-14 08:09:32 +0300

    Update addCustomerTest.java

--- cleaningService/src/test/java/cleaningService/addCustomerTest.java
+++ cleaningService/src/test/java/cleaningService/addCustomerTest.java
@@ -11,14 +11,18 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class addCustomerTest {
-	String name;
-	boolean a = true;
+ boolean a = false;
+	 boolean b = true;
+	 String username;
+	 String password;
+	 String name;
 	 String phone;
 	 String address;
 	 String email;
-	 String password;
 	 Scanner s=new Scanner(new InputStreamReader(System.in));
-	
+	 CustomerFunction cf = new CustomerFunction();
+	 Customer c = new Customer() ;
+	 
 
 	@Given("these are the customer")
 	public void theseAreTheCustomer(io.cucumber.datatable.DataTable dataTable) {
@@ -30,14 +34,29 @@ public class addCustomerTest {
 	    //
 	    // For other transformations you can register a DataTableType.
 
+		  
+		 for(int i=0; i < 4;i++) {
+			 username = dataTable.cell(i,1);
+			 password = dataTable.cell(i,2);
+			 name = dataTable.cell(i,3);
+			 phone = dataTable.cell(i,4);
+			 address = dataTable.cell(i,5);
+		//	 email = dataTable.cell(i,6);
+
+			 Customer c1 =  new Customer(username,password,name,address,phone);
+
+			 CustomerFunction.customers.add(c1);
+            
+			 
+		 }
 		 System.out.print("Enter your user name: ");
 		 name = s.nextLine();
-		 
-		 for(int i=0;i<3;i++)
+		 for(int i=0;i<CustomerFunction.customers.size();i++)
 			{
 				
-				if((	name.equalsIgnoreCase( dataTable.cell(i,1)) ) ) {
-					 a=false;
+				if(	username.equalsIgnoreCase( CustomerFunction.customers.get(i).getUsername()  )) {
+			//		System.out.print(cf.customers.get(i).getUsername());
+					 a=true;
 					
 				}
 				
@@ -60,19 +79,28 @@ public class addCustomerTest {
 	@Then("the customer can be added")
 	public void theCustomerCanBeAdded() {
 	    // Write code here that turns the phrase above into concrete actions
-	     assertEquals(true,a);
+	     assertEquals(false,a);
+		
+	//	 assertTrue(a==false);
 	}
 	@Then("the customer is added successful")
 	public void theCustomerIsAddedSuccessful() {
 	    // Write code here that turns the phrase above into concrete actions
+		 System.out.print("the password : ");
+		 password = s.nextLine();
+		 System.out.print("the name : ");
+		 name = s.nextLine();
 		 System.out.print("the phone: ");
 		 phone = s.nextLine();
-			 System.out.print("the address: ");
-			 address = s.nextLine();
-			 System.out.print("the email: ");
-			 email = s.nextLine();
-			 System.out.print("the password : ");
-			 password = s.nextLine();
+	     System.out.print("the address: ");
+		 address = s.nextLine();
+	     System.out.print("the email: ");
+	     email = s.nextLine();
+			
+	 //    Customer c1=new Customer(username,password,name,address,phone,email);
+	     Customer c1=new Customer(username,password,name,address,phone);
+
+		 CustomerFunction.customers.add( c1);
 			
 			 
 	}
@@ -88,7 +116,8 @@ public class addCustomerTest {
 	@Then("the customer cannot be added")
 	public void theCustomerCannotBeAdded() {
 	    // Write code here that turns the phrase above into concrete actions
-		 assertEquals(false,a);
+		 assertEquals(ture,a);
+		// assertTrue(a==true);
 	}
 	@Then("the customer is log in fail")
 	public void theCustomerIsLogInFail() {
